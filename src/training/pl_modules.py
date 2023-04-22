c6f5a8e3b7d826512dd4414be54c9ba1d68e1b85
Mykelo <michal.mieszko.wawrzyniak@gmail.com>
2023-04-22 15:52:34 +0200

    Update training script

--- src/training/pl_modules.py
+++ src/training/pl_modules.py
@@ -6,59 +6,63 @@ from sklearn.metrics import f1_score
 
 
 class LitDFL(pl.LightningModule):
-    def __init__(self):
+    def __init__(self, learning_rate: float = 1e-3):
         super().__init__()
 
         self.model = R3DDFL()
+        self.learning_rate = learning_rate
         self.training_step_outputs: list[dict[str, torch.Tensor]] = []
         self.validation_step_outputs: list[dict[str, torch.Tensor]] = []
         self.test_step_outputs: list[dict[str, torch.Tensor]] = []
 
+        self.save_hyperparameters()
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.model(x)
 
-    def training_step(
-        self, batch: list[torch.Tensor, torch.Tensor], batch_idx: int
-    ) -> torch.Tensor:
+    def on_train_start(self) -> None:
+        self.logger.log_hyperparams(self.hparams)
+
+    def training_step(self, batch: list[torch.Tensor], batch_idx: int) -> torch.Tensor:
         output_data = self._shared_eval_step(batch, batch_idx, "train")
         self.training_step_outputs.append(output_data)
 
         return output_data["loss"]
 
-    def test_step(self, batch: list[torch.Tensor, torch.Tensor], batch_idx: int):
+    def test_step(self, batch: list[torch.Tensor], batch_idx: int):
         outputs = self._shared_eval_step(batch, batch_idx, "test")
         self.test_step_outputs.append(outputs)
 
-    def validation_step(self, batch: list[torch.Tensor, torch.Tensor], batch_idx: int):
+    def validation_step(self, batch: list[torch.Tensor], batch_idx: int):
         outputs = self._shared_eval_step(batch, batch_idx, "val")
         self.validation_step_outputs.append(outputs)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
         return optimizer
 
     def on_train_epoch_end(self) -> None:
         f1 = self._calc_f1(self.training_step_outputs)
         self.training_step_outputs.clear()
-        self.logger.log_metrics({f"train_f1": f1})
+        self.log("train_f1", f1, prog_bar=True)
 
     def on_validation_epoch_end(self) -> None:
         f1 = self._calc_f1(self.validation_step_outputs)
         self.validation_step_outputs.clear()
-        self.logger.log_metrics({f"val_f1": f1})
+        self.log("val_f1", f1, prog_bar=True)
 
     def on_test_epoch_end(self) -> None:
         f1 = self._calc_f1(self.test_step_outputs)
         self.test_step_outputs.clear()
-        self.logger.log_metrics({f"test_f1": f1})
+        self.log("test_f1", f1)
 
     def _shared_eval_step(
-        self, batch: list[torch.Tensor, torch.Tensor], batch_idx: int, step: str
-    ) -> torch.Tensor:
+        self, batch: list[torch.Tensor], batch_idx: int, step: str
+    ) -> dict[str, torch.Tensor]:
         x, y = batch
         y_hat = self.model(x)
         loss = F.cross_entropy(y_hat, y)
-        self.logger.log_metrics(metrics={f"{step}_loss": loss.item()})
+        self.log(f"{step}_loss", loss.item())
 
         return {"loss": loss, "pred": y_hat, "true": y}
 
--- src/training/train_r3d.py
+++ src/training/train_r3d.py
@@ -1,32 +1,53 @@
 import typer
 import logging
+from typing import Optional
 from pathlib import Path
 import lightning.pytorch as pl
 from lightning.pytorch.loggers import MLFlowLogger
+from lightning.pytorch.callbacks import ModelCheckpoint
 from src.training.pl_modules import LitDFL
 from src.data.data_modules import DFLDataModule
 import torch
 
 
-def main():
+def main(
+    experiment_name: str = "res_net_3d",
+    data_dir: Path = typer.Option(exists=True, default=Path("data", "processed")),
+    checkpoint_dir: Path = typer.Option(
+        exists=True, dir_okay=True, default=Path("models")
+    ),
+    max_epochs: int = typer.Option(default=20),
+    random_state: Optional[int] = typer.Option(default=42),
+):
     """Trains the ResNet 3D model."""
     AVAIL_GPUS = min(1, torch.cuda.device_count())
 
-    mlf_logger = MLFlowLogger(experiment_name="res_net_3d")
+    logger = logging.getLogger("Training ResNet 3D model")
+    logger.info(f"Using {AVAIL_GPUS} GPUs")
+    logger.info(f"experiment_name = {experiment_name}")
+    logger.info(f"data_dir = {data_dir}")
+    logger.info(f"checkpoint_dir = {checkpoint_dir}")
+    logger.info(f"random_state = {random_state}")
+
+    mlf_logger = MLFlowLogger(experiment_name=experiment_name)
 
     model = LitDFL()
-    dm = DFLDataModule(
-        data_dir=str(Path("data", "processed")), random_state=42, batch_size=2
+    dm = DFLDataModule(data_dir=str(data_dir), random_state=random_state, batch_size=2)
+    checkpoint_callback = ModelCheckpoint(
+        monitor="val_f1",
+        dirpath=Path(checkpoint_dir, experiment_name),
+        filename=f"{experiment_name}_{mlf_logger.run_id}",
+        save_top_k=1,
+        mode="max",
     )
     trainer = pl.Trainer(
-        default_root_dir=Path("models", "r3d"),
+        default_root_dir=Path(checkpoint_dir, experiment_name),
         logger=mlf_logger,
         accelerator="cuda" if AVAIL_GPUS > 0 else "cpu",
         devices=AVAIL_GPUS,
-        max_epochs=5,
-        limit_train_batches=10,
-        limit_val_batches=10,
-        log_every_n_steps=1
+        callbacks=[checkpoint_callback],
+        max_epochs=max_epochs,
+        log_every_n_steps=1,
     )
 
     trainer.fit(model=model, datamodule=dm)
